3ab1689dd1c222722a4f31976e580d569b3646e0
ksmai <kasingmai@gmail.com>
2018-07-22 22:06:08 +0800

    Implement player class to implement with monster

--- player.cpp
+++ player.cpp
@@ -1,1 +1,19 @@
+#include <algorithm>
+
 #include "player.h"
+#include "monster.h"
+
+Player::Player(): hp(Player::FULL_HP), atk(Player::INITIAL_ATK) {
+}
+
+void Player::attack(Monster &monster) const {
+  monster.receive_damage(atk);
+}
+
+void Player::defend(const Monster &monster) {
+  hp = std::max(0, hp - monster.get_damage());
+}
+
+bool Player::is_dead() const {
+  return hp <= 0;
+}
--- player.h
+++ player.h
@@ -1,7 +1,19 @@
 #ifndef __PLAYER_H__
 #define __PLAYER_H__
 
+#include "monster.h"
+
 class Player {
+  int hp;
+  int atk;
+  static const int FULL_HP = 100;
+  static const int INITIAL_ATK = 5;
+
+public:
+  Player();
+  void attack(Monster &monster) const;
+  void defend(const Monster &monster);
+  bool is_dead() const;
 };
 
 #endif
