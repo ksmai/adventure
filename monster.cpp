54f3b7b3a4b3398c5f5557549eb2767a4ca0ab98
ksmai <kasingmai@gmail.com>
2018-07-22 22:05:44 +0800

    Implement random monster class

--- monster.cpp
+++ monster.cpp
@@ -1,1 +1,25 @@
+#include <algorithm>
+#include <random>
+
 #include "monster.h"
+
+Monster::Monster() {
+  std::random_device dev;
+  std::mt19937 mt(dev());
+  std::uniform_int_distribution<int> rand_hp(MIN_HP, MAX_HP);
+  std::uniform_int_distribution<int> rand_atk(MIN_ATK, MAX_ATK);
+  hp = rand_hp(mt);
+  atk = rand_atk(mt);
+}
+
+void Monster::receive_damage(int dmg) {
+  hp = std::max(0, hp - dmg);
+}
+
+int Monster::get_damage() const {
+  return atk;
+}
+
+bool Monster::is_dead() const {
+  return hp <= 0;
+}
--- monster.h
+++ monster.h
@@ -2,6 +2,18 @@
 #define __MONSTER_H__
 
 class Monster {
+  int hp;
+  int atk;
+  static const int MIN_HP = 5;
+  static const int MAX_HP = 25;
+  static const int MIN_ATK = 1;
+  static const int MAX_ATK = 5;
+
+public:
+  Monster();
+  void receive_damage(int dmg);
+  int get_damage() const;
+  bool is_dead() const;
 };
 
 #endif
