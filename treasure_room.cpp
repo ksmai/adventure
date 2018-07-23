d8e8f466e1bd8a4af43201d8687a01ad76b1060a
ksmai <kasingmai@gmail.com>
2018-07-23 22:18:57 +0800

    Implement TreasureRoom

--- treasure_room.cpp
+++ treasure_room.cpp
@@ -1,1 +1,14 @@
+#include <iostream>
+
 #include "treasure_room.h"
+
+bool TreasureRoom::inspect(Player &p) {
+  std::cout << "There is a treasure box in the middle of the room.\n";
+  std::cout << "Open it? (Y/N) Y\n";
+  std::cout << "You found a lot of money in the box!!!\n\n";
+  return true;
+}
+
+bool TreasureRoom::try_leave(Player &p) {
+  return true;
+}
--- treasure_room.h
+++ treasure_room.h
@@ -2,8 +2,12 @@
 #define __TREASURE_ROOM_H__
 
 #include "room.h"
+#include "player.h"
 
 class TreasureRoom: public Room {
+public:
+  virtual bool inspect(Player &p);
+  virtual bool try_leave(Player &p);
 };
 
 #endif
