6ff0b83e273605699c74035e8df6f53893e56232
ksmai <kasingmai@gmail.com>
2018-07-23 22:39:27 +0800

    Implement Game

--- game.cpp
+++ game.cpp
@@ -1,1 +1,53 @@
+#include <iostream>
+#include <limits>
+
 #include "game.h"
+#include "player.h"
+#include "room.h"
+
+Game::Game():
+  rooms(10),
+  monster_rooms(5),
+  treasure_rooms(1),
+  current_room(&rooms[0]) {
+}
+
+bool Game::is_win() const {
+  return won;
+}
+
+bool Game::is_lose() const {
+  return player.is_dead();
+}
+
+void Game::next_round() {
+  char c;
+  std::cout << "What will you do? ";
+  std::cin >> c;
+  std::cin.clear();
+  std::cin.ignore(std::numeric_limits<std::streamsize>::max(), '\n');
+  switch (c) {
+    case 'W':
+    case 'w':
+      current_room->go_top(player);
+      break;
+    case 'A':
+    case 'a':
+      current_room->go_left(player);
+      break;
+    case 'S':
+    case 's':
+      current_room->go_bottom(player);
+      break;
+    case 'D':
+    case 'd':
+      current_room->go_right(player);
+      break;
+    case 'I':
+    case 'i':
+      won = current_room->inspect(player);
+      break;
+    default:
+      std::cout << "WASD for movement. I for inspect.\n\n";
+  }
+}
--- game.h
+++ game.h
@@ -1,7 +1,26 @@
 #ifndef __GAME_H__
 #define __GAME_H__
 
+#include <vector>
+
+#include "room.h"
+#include "monster_room.h"
+#include "treasure_room.h"
+#include "player.h"
+
 class Game {
+  bool won;
+  Player player;
+  std::vector<Room> rooms;
+  std::vector<MonsterRoom> monster_rooms;
+  std::vector<TreasureRoom> treasure_rooms;
+  Room *current_room;
+
+public:
+  Game();
+  bool is_lose() const;
+  bool is_win() const;
+  void next_round();
 };
 
 #endif
